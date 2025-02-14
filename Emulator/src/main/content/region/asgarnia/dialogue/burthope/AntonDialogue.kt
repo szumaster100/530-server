69c40b586e89ed11fcee4f816e36ffa28d6e8699
szumaster <szumaster2009@protonmail.com>
2025-02-14 22:48:34 +0100

    Fixed import for AntonDialogue

--- Emulator/src/main/content/region/asgarnia/dialogue/burthope/AntonDialogue.kt
+++ Emulator/src/main/content/region/asgarnia/dialogue/burthope/AntonDialogue.kt
@@ -1,6 +1,6 @@
 package content.region.asgarnia.dialogue.burthope
 
-import core.api.openNpcShop
+import core.api.interaction.openNpcShop
 import core.game.dialogue.Dialogue
 import core.game.dialogue.FaceAnim
 import core.game.node.entity.npc.NPC
